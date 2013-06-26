9456d48794803829220eb44620eb48f56e6aa58a
Inna Mihailova <innna.p.m@gmail.com>
2013-06-26 18:34:02 +0300

    create model User

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Tengag::Application.routes.draw do
+  devise_for :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe User do
+  it { should allow_mass_assignment_of(:email) }
+  it { should allow_mass_assignment_of(:password) }
+  it { should allow_mass_assignment_of(:password_confirmation) }
+  it { should allow_mass_assignment_of(:remember_me) }
+end
