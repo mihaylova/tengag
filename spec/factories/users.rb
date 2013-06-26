7c07c32ec96d64ed7c4a44676f4518412378c456
Inna Mihailova <innna.p.m@gmail.com>
2013-06-26 23:38:17 +0300

    use username or email for user login

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,15 @@
 
 FactoryGirl.define do
   factory :user do
+    sequence :email do |n|
+      "person#{n}@example.com"
+    end
+
+    sequence :username do |n|
+      "user#{n}"
+    end
+
+    password 12345678
+    password_confirmation 12345678
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,34 @@ require 'spec_helper'
 
 describe User do
   it { should allow_mass_assignment_of(:email) }
+
   it { should allow_mass_assignment_of(:password) }
+
   it { should allow_mass_assignment_of(:password_confirmation) }
+
   it { should allow_mass_assignment_of(:remember_me) }
+
+  it { should allow_mass_assignment_of(:username) }
+
+  it { should respond_to(:login) }
+  it { should respond_to(:login=) }
+  it { should allow_mass_assignment_of(:login) }
+
+  context 'login' do
+    before do
+      @user = FactoryGirl.create :user
+    end
+
+    its "can find user by username" do
+      user = User.find_first_by_auth_conditions({login: @user.username})
+
+      user.should eq @user
+    end
+
+    its "can find user by email" do
+      user = User.find_first_by_auth_conditions({login: @user.email})
+
+      user.should eq @user
+    end
+  end
 end
