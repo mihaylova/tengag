f819c06fe1e02668ee44078dec374556e81fa1fb
Inna Mihailova <innna.p.m@gmail.com>
2013-06-26 17:45:53 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tengag::Application.config.secret_token = '269b145ce29119e0435798ba1a5298f4679c3a6c4a4c85af975fd35226c99805d109c2dd7aef2629db484e64e8ca65a6d02bd6be8dea943e1d2b9c74e695f150'
