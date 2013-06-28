eb4a9d5f955abbb1005a1cefcde2c9588149fa2b
Inna Mihailova <innna.p.m@gmail.com>
2013-06-28 12:14:16 +0300

    add tests for posts

---
+++ spec/features/posts_spec.rb
@@ -0,0 +1,80 @@
+require 'spec_helper'
+
+describe "Posts:" do
+  let(:post) { FactoryGirl.build(:post, id: 1, created_at: DateTime.now, updated_at: DateTime.now) }
+ 
+    it 'User can see posts' do
+      Post.should_receive(:all).and_return([post])
+
+      visit posts_path
+      page.should have_content(post.title)
+    end
+
+  context 'When user is logged' do 
+    let(:user) { FactoryGirl.create(:user) }
+
+    before :each do
+     login_as(user, :scope => :user)
+    end
+
+    after(:each) { Warden.test_reset!  }
+
+    it 'can see single post' do
+      Post.should_receive(:find).and_return(post)
+
+      visit post_path(post)
+      page.should have_content(post.title)
+    end
+
+    it 'can create post' do
+      expect do
+        visit new_post_path
+
+        fill_in 'Title', with: "Some post title"
+        click_on 'Create Post'
+      end.to change { Post.count }.by(1)
+    end
+
+    it 'can update post' do
+      Post.should_receive(:find).exactly(3).times.and_return(post)
+      # Post.any_instance.stub(:update_attributes) { true }
+      # Post.any_instance.stub(:new_record?) { false }
+      Post.any_instance.stub(update_attributes: true, new_record?: false)
+
+      visit edit_post_path(post)
+      click_on 'Update Post'
+
+      page.current_path.should == post_path(post)
+    end
+
+    it 'can delete post' do
+      Post.should_receive(:all).and_return([post])
+
+      visit posts_path
+      page.should have_content('Delete')
+    end
+
+  end
+
+  context 'When user is not logged' do
+
+    it 'can not see a post' do
+      Post.stub(:find) { post }
+      visit post_path(post)
+      page.should_not have_content(post.title)
+    end
+
+    it 'can not create post' do
+      visit new_post_path
+      current_path.should == new_user_session_path
+    end
+
+    it 'can not update post' do
+      Post.stub(:find) { post }
+      visit edit_post_path(post)
+      current_path.should == new_user_session_path
+    end
+
+
+  end
+end
---
+++ spec/features/root_url_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe "Root URL" do
+  it 'body should eq to posts#index body'do
+    visit posts_path
+    posts_body = page.body
+    
+    visit root_path
+    page.body.should eq posts_body
+  end
+end
